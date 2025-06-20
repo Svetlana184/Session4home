dbe5f81555a70a7c7321b38f3a134cd98aa5cc07
Svetlana184 <sveta.kgd.39@mail.com>
2025-06-20 23:16:50 +0200

    evs

---
+++ Session4/Model/Newss.cs
@@ -0,0 +1,10 @@
+﻿namespace Session4.Model
+{
+    public class Newss
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
